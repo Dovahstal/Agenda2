d2d9564b067ff6f98d1f5a5a8cbb6fc29672e19f
Zerkyss <vincent.varinot@saintmichelannecy.fr>
2024-04-04 21:05:57 +0200

    dao event ok

--- Service/DAO/DAO_events.cs
+++ Service/DAO/DAO_events.cs
@@ -11,43 +11,43 @@ namespace Agenda2.Service.DAO
 {
     class DAO_events
     {
-        ////Récupére tout les événements de la database
-        //public IEnumerable<Evenement> GetAllEvenements()
-        //{
-        //    using (var Context = new EvenementLongContext())
-        //    {
-        //        var AllEvents = Context.Evenements.ToList();
-        //        return AllEvents;
-        //    }
-        //}
+        //Récupére tout les événements de la database
+        public IEnumerable<Evenement> GetAllEvenements()
+        {
+            using (var Context = new ContactLongContext())
+            {
+                var AllEvents = Context.Evenements.ToList();
+                return AllEvents;
+            }
+        }
 
-        ////Ajoute un événement à la database
-        //public string AddEvenement(Evenement evenement)
-        //{
-        //    using (var Context = new EvenementLongContext())
-        //    {
-        //        Context.Evenements.Add(evenement);
-        //        Context.SaveChanges();
-        //        return "Evenement ajouté";
-        //    }
-        //}
+        //Ajoute un événement à la database
+        public string AddEvenement(Evenement evenement)
+        {
+            using (var Context = new ContactLongContext())
+            {
+                Context.Evenements.Add(evenement);
+                Context.SaveChanges();
+                return "Evenement ajouté";
+            }
+        }
 
-        //public string DeleteEvenement(int ID)
-        //{
-        //    using (var Context = new EvenementLongContext())
-        //    {
-        //        var itemToRemove = Context.Contacts.SingleOrDefault(x => x.Idcontacts == ID);
-        //        if (itemToRemove != null)
-        //        {
-        //            Context.Contacts.Remove(itemToRemove);
-        //            Context.SaveChanges();
-        //            return "Artiste supprimé";
-        //        }
-        //        else
-        //        {
-        //            return "Artiste non trouvé";
-        //        }
-        //    }
-        //}
+        public string DeleteEvenement(int ID)
+        {
+            using (var Context = new ContactLongContext())
+            {
+                var itemToRemove = Context.Contacts.SingleOrDefault(x => x.Idcontacts == ID);
+                if (itemToRemove != null)
+                {
+                    Context.Contacts.Remove(itemToRemove);
+                    Context.SaveChanges();
+                    return "Artiste supprimé";
+                }
+                else
+                {
+                    return "Artiste non trouvé";
+                }
+            }
+        }
     }
 }
