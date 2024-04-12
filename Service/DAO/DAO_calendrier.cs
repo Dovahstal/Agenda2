1bf1080233e3da941a7b73001a0c40ce45142287
matheoalexis long <matheoalexis.long@saintmichelannecy.fr>
2024-04-12 10:42:13 +0200

    enfaite non

--- Service/DAO/DAO_calendrier.cs
+++ Service/DAO/DAO_calendrier.cs
@@ -10,58 +10,58 @@ using Google.Apis.Services;
 using Agenda2.Agenda2DB;
 
 
-namespace Agenda2.Service.DAO
-{ 
-    class DAO_calendrier
-    {
-        static void Main(string[] args)
-        {
-            //chemin vers le fichier json des identifiants
-            string cheminVersFichierJson = "C:\\Users\\SLAB71\\source\\repos\\Dovahstal\\Agenda2\\Ressources\\Identifiants Agenda Long.json";
+//namespace Agenda2.Service.DAO
+//{ 
+//    class DAO_calendrier
+//    {
+//        static void Main(string[] args)
+//        {
+//            //chemin vers le fichier json des identifiants
+//            string cheminVersFichierJson = "C:\\Users\\SLAB71\\source\\repos\\Dovahstal\\Agenda2\\Ressources\\Identifiants Agenda Long.json";
 
-            //initialisation de l'authentification
-            GoogleCredential credential;
-            using (var stream = new System.IO.FileStream(cheminVersFichierJson, System.IO.FileMode.Open, System.IO.FileAccess.Read))
-            {
-                credential = GoogleCredential.FromStream(stream)
-                    .CreateScoped(CalendarService.Scope.Calendar);
-            }
+//            //initialisation de l'authentification
+//            GoogleCredential credential;
+//            using (var stream = new System.IO.FileStream(cheminVersFichierJson, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+//            {
+//                credential = GoogleCredential.FromStream(stream)
+//                    .CreateScoped(CalendarService.Scope.Calendar);
+//            }
 
-            // Création du service Google Calendar
-            var service = new CalendarService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "Application Agenda",
-            });
+//            // Création du service Google Calendar
+//            var service = new CalendarService(new BaseClientService.Initializer()
+//            {
+//                HttpClientInitializer = credential,
+//                ApplicationName = "Application Agenda",
+//            });
 
-            // Récupération des événements à venir
-            EventsResource.ListRequest request = service.Events.List("primary");
-            request.TimeMin = DateTime.Now;
-            request.ShowDeleted = false;
-            request.SingleEvents = true;
-            request.MaxResults = 10;
-            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+//            // Récupération des événements à venir
+//            EventsResource.ListRequest request = service.Events.List("primary");
+//            request.TimeMin = DateTime.Now;
+//            request.ShowDeleted = false;
+//            request.SingleEvents = true;
+//            request.MaxResults = 10;
+//            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
 
-            // Exécution de la requête
-            Events events = request.Execute();
-            Console.WriteLine("Événements à venir :");
-            if (events.Items != null && events.Items.Count > 0)
-            {
-                foreach (var eventItem in events.Items)
-                {
-                    string when = eventItem.Start.DateTime.ToString();
-                    if (string.IsNullOrEmpty(when))
-                    {
-                        when = eventItem.Start.Date;
-                    }
-                    Console.WriteLine($"{eventItem.Summary} (Début : {when})");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Aucun événement à venir trouvé.");
-            }
-            Console.ReadLine();
-        }
-    }
-}
+//            // Exécution de la requête
+//            Events events = request.Execute();
+//            Console.WriteLine("Événements à venir :");
+//            if (events.Items != null && events.Items.Count > 0)
+//            {
+//                foreach (var eventItem in events.Items)
+//                {
+//                    string when = eventItem.Start.DateTime.ToString();
+//                    if (string.IsNullOrEmpty(when))
+//                    {
+//                        when = eventItem.Start.Date;
+//                    }
+//                    Console.WriteLine($"{eventItem.Summary} (Début : {when})");
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Aucun événement à venir trouvé.");
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
