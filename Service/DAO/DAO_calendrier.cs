dbce4fd8a0ec9d80741d8c66c18fdf6af4efccd2
matheoalexis long <matheoalexis.long@saintmichelannecy.fr>
2024-04-12 10:43:56 +0200

    test

--- Service/DAO/DAO_calendrier.cs
+++ Service/DAO/DAO_calendrier.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Calendar.v3;
-using Google.Apis.Calendar.v3.Data;
-using Google.Apis.Services;
-using Agenda2.Agenda2DB;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Google.Apis.Auth.OAuth2;
+//using Google.Apis.Calendar.v3;
+//using Google.Apis.Calendar.v3.Data;
+//using Google.Apis.Services;
+//using Agenda2.Agenda2DB;
 
 
 //namespace Agenda2.Service.DAO
